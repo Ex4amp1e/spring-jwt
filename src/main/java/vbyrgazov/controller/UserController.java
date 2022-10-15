a371fc2c2bfd2e85d83b332da4bbec79522a086e
Vladislav Byrgazov <vladislav.byrgazov@spirent.com>
2022-10-15 17:41:23 +0500

    Updated UserController.java by new endpoints

--- src/main/java/vbyrgazov/controller/UserController.java
+++ src/main/java/vbyrgazov/controller/UserController.java
@@ -1,14 +1,22 @@
 package vbyrgazov.controller;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import vbyrgazov.model.Role;
 import vbyrgazov.model.User;
 import vbyrgazov.service.UserService;
 
+import java.net.URI;
 import java.util.List;
+
+@Data
+class RoleToUser {
+    String username;
+    String roleName;
+}
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +27,23 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUser form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
     }
 }
